6b3cb9b2b93e34887e6fce3f997318335c02df8d
unknown <ajeeshavi@gmail.com>
2019-10-14 09:06:02 +0900

    [artf2330] Update the header files

--- Inc/sensor.h
+++ Inc/sensor.h
@@ -12,13 +12,9 @@ typedef enum
     UART_UNK
 
 }Uart_Statemachine;
-static Uart_Statemachine my_uart_se=UART_CHECK;
+
 /* USER CODE BEGIN EM */
-char rx_buffer_gps[25];
-uint8_t rxData_ledon[32]="ONE RECEIVED SO LED WILL ON\r\n";
-uint8_t rxData_ledoz[33]="ZERO RECEIVED SO LED WILL OFF\r\n";
-uint8_t rxData_ledou[17]="UNKNOWN ENTRY\r\n";
-uint8_t rxData_wrong[24]="WRONG DATA...........\r\n";
+
 /* USER CODE END EM */
 void message_handle(char *msg);
 void gps_valuereceive(void);
