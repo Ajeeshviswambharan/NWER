51895f462031240c7ccc74524ac9bb4c6488237d
unknown <ajeeshavi@gmail.com>
2020-05-09 17:16:57 +0530

    update ignore

---
+++ Debug/Src/stm32f0xx_it.d
@@ -0,0 +1,115 @@
+Src/stm32f0xx_it.o: ../Src/stm32f0xx_it.c \
+ F:/Project/Projects_STM32/NWER/Inc/main.h \
+ F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h \
+ F:/Project/Projects_STM32/NWER/Inc/stm32f0xx_hal_conf.h \
+ F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc.h \
+ F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_def.h \
+ F:/Project/Projects_STM32/NWER/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h \
+ F:/Project/Projects_STM32/NWER/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f030x8.h \
+ F:/Project/Projects_STM32/NWER/Drivers/CMSIS/Include/core_cm0.h \
+ F:/Project/Projects_STM32/NWER/Drivers/CMSIS/Include/core_cmInstr.h \
+ F:/Project/Projects_STM32/NWER/Drivers/CMSIS/Include/cmsis_gcc.h \
+ F:/Project/Projects_STM32/NWER/Drivers/CMSIS/Include/core_cmFunc.h \
+ F:/Project/Projects_STM32/NWER/Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h \
+ F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc_ex.h \
+ F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio.h \
+ F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio_ex.h \
+ F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma.h \
+ F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h \
+ F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_cortex.h \
+ F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash.h \
+ F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h \
+ F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c.h \
+ F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c_ex.h \
+ F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr.h \
+ F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr_ex.h \
+ F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_tim.h \
+ F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_tim_ex.h \
+ F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart.h \
+ F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart_ex.h \
+ F:/Project/Projects_STM32/NWER/Inc/stm32f0xx_it.h \
+ F:/Project/Projects_STM32/NWER/Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h \
+ F:/Project/Projects_STM32/NWER/Inc/FreeRTOSConfig.h \
+ F:/Project/Projects_STM32/NWER/Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h \
+ F:/Project/Projects_STM32/NWER/Middlewares/Third_Party/FreeRTOS/Source/include/portable.h \
+ F:/Project/Projects_STM32/NWER/Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h \
+ F:/Project/Projects_STM32/NWER/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM0/portmacro.h \
+ F:/Project/Projects_STM32/NWER/Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h \
+ F:/Project/Projects_STM32/NWER/Middlewares/Third_Party/FreeRTOS/Source/include/task.h \
+ F:/Project/Projects_STM32/NWER/Middlewares/Third_Party/FreeRTOS/Source/include/list.h
+
+F:/Project/Projects_STM32/NWER/Inc/main.h:
+
+F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h:
+
+F:/Project/Projects_STM32/NWER/Inc/stm32f0xx_hal_conf.h:
+
+F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc.h:
+
+F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_def.h:
+
+F:/Project/Projects_STM32/NWER/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h:
+
+F:/Project/Projects_STM32/NWER/Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f030x8.h:
+
+F:/Project/Projects_STM32/NWER/Drivers/CMSIS/Include/core_cm0.h:
+
+F:/Project/Projects_STM32/NWER/Drivers/CMSIS/Include/core_cmInstr.h:
+
+F:/Project/Projects_STM32/NWER/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+F:/Project/Projects_STM32/NWER/Drivers/CMSIS/Include/core_cmFunc.h:
+
+F:/Project/Projects_STM32/NWER/Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
+
+F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc_ex.h:
+
+F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio.h:
+
+F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_gpio_ex.h:
+
+F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma.h:
+
+F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_dma_ex.h:
+
+F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_cortex.h:
+
+F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash.h:
+
+F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_flash_ex.h:
+
+F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c.h:
+
+F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_i2c_ex.h:
+
+F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr.h:
+
+F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_pwr_ex.h:
+
+F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_tim.h:
+
+F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_tim_ex.h:
+
+F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart.h:
+
+F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart_ex.h:
+
+F:/Project/Projects_STM32/NWER/Inc/stm32f0xx_it.h:
+
+F:/Project/Projects_STM32/NWER/Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h:
+
+F:/Project/Projects_STM32/NWER/Inc/FreeRTOSConfig.h:
+
+F:/Project/Projects_STM32/NWER/Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h:
+
+F:/Project/Projects_STM32/NWER/Middlewares/Third_Party/FreeRTOS/Source/include/portable.h:
+
+F:/Project/Projects_STM32/NWER/Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h:
+
+F:/Project/Projects_STM32/NWER/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM0/portmacro.h:
+
+F:/Project/Projects_STM32/NWER/Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h:
+
+F:/Project/Projects_STM32/NWER/Middlewares/Third_Party/FreeRTOS/Source/include/task.h:
+
+F:/Project/Projects_STM32/NWER/Middlewares/Third_Party/FreeRTOS/Source/include/list.h:
---
+++ Debug/Src/subdir.mk
@@ -0,0 +1,46 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/freertos.c \
+../Src/main.c \
+../Src/stm32f0xx_hal_msp.c \
+../Src/stm32f0xx_hal_timebase_tim.c \
+../Src/stm32f0xx_it.c \
+../Src/syscalls.c \
+../Src/system_stm32f0xx.c \
+../Src/user.c 
+
+OBJS += \
+./Src/freertos.o \
+./Src/main.o \
+./Src/stm32f0xx_hal_msp.o \
+./Src/stm32f0xx_hal_timebase_tim.o \
+./Src/stm32f0xx_it.o \
+./Src/syscalls.o \
+./Src/system_stm32f0xx.o \
+./Src/user.o 
+
+C_DEPS += \
+./Src/freertos.d \
+./Src/main.d \
+./Src/stm32f0xx_hal_msp.d \
+./Src/stm32f0xx_hal_timebase_tim.d \
+./Src/stm32f0xx_it.d \
+./Src/syscalls.d \
+./Src/system_stm32f0xx.d \
+./Src/user.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/%.o: ../Src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m0 -mthumb -mfloat-abi=soft '-D__weak=__attribute__((weak))' '-D__packed=__attribute__((__packed__))' -DUSE_HAL_DRIVER -DSTM32F030x8 -I"F:/Project/Projects_STM32/NWER/Inc" -I"F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc" -I"F:/Project/Projects_STM32/NWER/Drivers/STM32F0xx_HAL_Driver/Inc/Legacy" -I"F:/Project/Projects_STM32/NWER/Middlewares/Third_Party/FreeRTOS/Source/include" -I"F:/Project/Projects_STM32/NWER/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS" -I"F:/Project/Projects_STM32/NWER/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM0" -I"F:/Project/Projects_STM32/NWER/Drivers/CMSIS/Device/ST/STM32F0xx/Include" -I"F:/Project/Projects_STM32/NWER/Drivers/CMSIS/Include"  -Og -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
